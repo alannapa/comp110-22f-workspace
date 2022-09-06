6d34cd4504612fb1c10e809f7c12d4616a9a45fa
alanna paschall <apaschall@unc.edu>
2022-09-06 15:20:06 -0400

    Finished Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -5,7 +5,7 @@ __author__ = "730487405"
 secret: str = "python"
 word: str = input(f"What is your {len(secret)}-letter guess? ")
 while len(word) != len(secret):
-    word: str = input(f"That was not {len(secret)} letters! Try again: ")
+    word = input(f"That was not {len(secret)} letters! Try again: ")
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -18,20 +18,20 @@ all_indices: int = 0
 
 while count < len(secret):
     if word[count] == secret[count]:
-        emojis: str = emojis + GREEN_BOX
+        emojis = emojis + GREEN_BOX
     else:
-        while anywhere != True and all_indices < len(secret):
+        while anywhere is not True and all_indices < len(secret):
             if word[count] == secret[all_indices]:
-                anywhere: bool = True
+                anywhere = True
             else:
-                all_indices: int = all_indices + 1
-        if anywhere == True:
-            emojis: str = emojis + YELLOW_BOX
+                all_indices = all_indices + 1
+        if anywhere is True:
+            emojis = emojis + YELLOW_BOX
         else:
-            emojis: str = emojis + WHITE_BOX
-    count: int = count + 1
-    anywhere: bool = False
-    all_indices: int = 0
+            emojis = emojis + WHITE_BOX
+    count = count + 1
+    anywhere = False
+    all_indices = 0
 
 print(emojis)
 if len(word) == len(secret):
