00de3be1e5339b944c4717680bb64d808f1a4903
alanna paschall <apaschall@unc.edu>
2022-09-18 15:41:51 -0400

    Resubmit of EX04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,8 +2,12 @@
 
 __author__ = "730487405"
 
+
 def all(haystack: list[int], needle: int) -> bool:
+    """Checks if one character is the same as the entire list"""
     i: int = 0
+    if len(haystack) == 0:
+        return False
     while i < len(haystack):
         if haystack[i] == needle:
             i += 1
@@ -13,16 +17,18 @@ def all(haystack: list[int], needle: int) -> bool:
 
 
 def max(argument: list[int]) -> int:
+    """Finds the maximum of a list without using max function."""
     if len(argument) == 0:
         raise ValueError("max() arg is an empty List")
+    m: int = 0
     n: int = 1
     while n < len(argument):
-        if argument[n] > argument[0]:
+        if argument[n] > argument[m]:
             n += 1
-            if argument[n] > argument [1]:
+            if argument[n] > argument [m]:
                 return argument[n]
             else:
-                return argument [1]
+                return argument [m]
         else:
             n += 1
             if argument [n] > argument [0]:
@@ -31,6 +37,7 @@ def max(argument: list[int]) -> int:
 
 
 def is_equal(first: list[int], second: list[int]) -> bool:
+    """Checks that two lists match each other exactly."""
     a: int = 0
     while a < len(first) and len(second):
         if first[a] == second[a]:
