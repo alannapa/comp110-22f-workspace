75cfb5ee3cdb26df75098ea02bdf564a317e91c4
alanna paschall <apaschall@unc.edu>
2022-09-26 13:54:50 -0400

    resubmit of EX05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,4 +1,4 @@
-"""EX05 - Skeleton function. """
+"""EX05 - Skeleton function."""
 
 __author__ = "730487405"
 
@@ -8,7 +8,7 @@ def only_evens(argument: list[int]) -> list[int]:
     i: int = 0
     output: list[int] = []
     while i < len(argument):
-        if argument[i]%2 == 0:
+        if argument[i] % 2 == 0:
             output.append(argument[i])
         i += 1
     return output
@@ -30,6 +30,12 @@ def concat(x: list[int], y: list[int]) -> list[int]:
 
 def sub(a_list: list[int], a: int, z: int) -> list[int]:
     """Returns a list as a subset of the given list."""
+    if len(a_list) == 0 or a > len(a_list) or z <= 0:
+        return []
+    if a < 0:
+        a = 0
+    if z > len(a_list):
+        z = len(a_list)
     outcome: list[int] = []
     z -= 1
     outcome.append(a_list[a])
