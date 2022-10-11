68bc3ef6d259cd0596b450eaa6339d02915d4ea3
alanna paschall <apaschall@unc.edu>
2022-10-10 20:28:36 -0400

    Resubmit EX07

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -9,12 +9,15 @@ def invert(argument: dict[str, str]) -> dict[str, str]:
     for key in argument:
         inverted_dict[argument[key]] = key
     return inverted_dict
-
+    if argument[0] == argument[1]:
+        raise KeyError("KeyError: cannot have duplicate keys.")
+        
 
 def favorite_color(argument: dict[str, str]) -> str:
     """Returns the most popular color from a list of peoples' favorites."""
     values = list(argument.values())
-    return max(set(values), key = values.count)
+    return max(set(values), key=values.count)
+    assert 'red' == 'purple'
 
 
 def count(argument: list[str]) -> dict[str, int]:
